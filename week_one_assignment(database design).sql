14299018ffeb970bb2909615767467acf0243f8f
Brian Kiptoo <bkiptoo553@gmail.com>
2025-04-17 09:40:15 +0300

    Add files via upload

---
+++ week_one_assignment(database design).sql
@@ -0,0 +1,7 @@
+-- question One PLP
+CREATE DATABASE salesDB;
+
+-- the database demo is created this way (CREATE DATABASE demo;)
+
+-- question Two PLP 
+DROP DATABASE demo; -- deletes the database demo
